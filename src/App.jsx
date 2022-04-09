0fcb1ac3c07de5c1ac5e77a8f6bb33962497fffe
Rozandi1 <98866052+Rozandi1@users.noreply.github.com>
2022-04-10 02:30:57 +0800

    memperbaiki card

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import Wisata from "./pages/Wisata/Wisata";
 import Kuliner from "./pages/Kuliner/Kuliner";
 import LandingPage from "./pages/Landing-page/LandingPage";
 import NotFound from "./pages/NotFound/NotFound";
-import Details from "./pages/Details";
+import Artikel from "./pages/Artikel/Artikel";
 
 function App() {
   return (
     <Routes>
-      <Route path='/LandingPage' element={<LandingPage />} />
-      <Route path='/' element={<Home />} />
-      <Route path='/Wisata' element={<Wisata />} />
-      <Route path='/Wisata/:wisataId' element={<Details />} />
-      <Route path='/Budaya' element={<Budaya />} />
-      <Route path='/Kuliner' element={<Kuliner />} />
-      <Route path='*' element={<NotFound />} />
+      <Route path="/LandingPage" element={<LandingPage />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/Budaya" element={<Budaya />} />
+      <Route path="/Kuliner" element={<Kuliner />} />
+      <Route path="/Wisata" element={<Wisata />} />
+      <Route path="/Artikel" element={<Artikel />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
